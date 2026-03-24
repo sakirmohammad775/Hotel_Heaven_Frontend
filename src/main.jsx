0af25dc4227b33c66b4b6251a975b7ca7b3d4cfe
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-03-24 16:38:39 +0600

    reuse my code because of error

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
+import AppRoutes from "./routes/AppRoutes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App></App>
+      <AppRoutes></AppRoutes>
     </BrowserRouter>
   </StrictMode>
 );
